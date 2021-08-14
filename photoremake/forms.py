b04d7a0b2c194efd850a8829afcb2ae2de13950c
riku <iamro1216@icloud.com>
2021-08-14 16:24:53 +0900

    レナねえに背景画像のドッキングは任せた！

--- photoremake/forms.py
+++ photoremake/forms.py
@@ -1,16 +1,11 @@
 from django import forms
 from django.db.models import fields
 
-from .models import Coordinated, Photo
+from .models import Photo
 
 class UploadForm(forms.ModelForm):
+    
 
   class Meta:
     model = Photo
-    fields = ('user', 'photo')
-
-
-class CoordinateForm(forms.ModelForm):
-    class Meta:
-        model = Coordinated
-        fields = ('back_id', 'title')
+    fields = ('user', 'photo', 'title')
--- photoremake/views.py
+++ photoremake/views.py
@@ -1,11 +1,16 @@
-from photoremake.models import Photo, Back, Coordinated
+from photoremake.models import Photo
 from django.shortcuts import render
 from django.views import generic
-from .forms import UploadForm, CoordinateForm
+from .forms import UploadForm
 from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse
+import numpy as np
 import cv2
 from PIL import Image, ImageDraw, ImageFilter
-
+import io
+import base64
+from django.conf import settings
+import os
 
 # 関数型でしか書いたことないから関数でまず書くわ
 
@@ -17,61 +22,55 @@ face_cascade_path = '/usr/local/opt/opencv/share/'\
                    'OpenCV/haarcascades/haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(face_cascade_path) #顔認識
 
+
 def upload_photo(request):
-    if request.method == "POST":
-        form = UploadForm(request.POST)
+    obj = Photo.objects.all()
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES, request.POST)
         if form.is_valid():
-            photo = Photo()
-            print(request)
-            photo.user = request.user.id
-            photo.photo = request.FILES['photo']
-            photo.save()
-            return redirect('/')
+            form.save()
+            print("セーブ完了")
+            return redirect('photoremake:index')
     else:
         form = UploadForm()
-    return render(request, 'upload.html', {'form': form})
+        obj = Photo.objects.all()
 
-def photo2image(back, photo):
-    #加工部分
-    im1 = Image.open(back) #ベースになる写真
-    im2 = Image.open(photo) #顔だけくり抜く時に使用
-    src = cv2.imread(photo) #顔の位置を判別するために使用
-    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(src_gray)
-            # 顔認識
+        #エラー処理のつもり🥺
+        if obj.exists() == False:
+            return render(request, 'upload.html', {'form': form,'obj':obj,})
+        
+        max_id = Photo.objects.latest('id').id
+        obj = Photo.objects.get(id = max_id)
+        x = settings.BASE_DIR + "/" + obj.photo.url
+        # print(x)
+        # img = cv2.imread(x)
+        # print(img)
+        y = settings.BASE_DIR + "/" + obj.photo.url
+        gray(x,y)
+
+    return render(request, 'upload.html', {
+        'form': form,
+        'obj':obj,
+    })
+
+
+###########ここをカスタマイズ############
+
+def gray(input_path,output_path):
+    img = cv2.imread(input_path)
+    back_img = settings.BASE_DIR + "/images/aogaku.jpg"
+    back = cv2.imread(back_img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #cv2.imwrite(output_path, img_gray)
+
+    faces = face_cascade.detectMultiScale(img_gray)
 
     for x, y, w, h in faces:
-        mask_im = Image.new("L", im2.size, 0) # Lがよく分からない
+        mask_im = Image.new("L", img.size, 0) # Lがよく分からない
         draw = ImageDraw.Draw(mask_im)
         draw.rectangle((x, y, x+w, y+h), fill=255) # 写真を顔の位置だけくり抜く
+    back = img.copy()
+    back.paste(img, (0, 0), mask_im) #im1にくり抜いた写真を貼り付け
+    cv2.putText(back,'AOYAMAGAKUIN',(20, 500),cv2.FONT_HERSHEY_COMPLEX,3,(255,0,255),4, lineType=cv2.LINE_AA) #文字書く！
+    cv2.imwrite(output_path, back) #保存
 
-    im1.paste(im2, (0, 0), mask_im) #im1にくり抜いた写真を貼り付け
-    cv2.putText(im1,'AOYAMAGAKUIN',(20, 500),cv2.FONT_HERSHEY_COMPLEX,3,(255,0,255),4, lineType=cv2.LINE_AA) #文字書く！
-    return im1
-
-def coordinate(request, pk):
-    p = get_object_or_404(Photo, pk=pk)
-    backs = Back.objects.all()
-
-    if request.method == "POST":
-        form = CoordinateForm(request.POST)
-        if form.is_valid():
-            print("formは正しいです。")
-            coordinate = Coordinated()
-            coordinate.photo_id = p
-            coordinate.title = request.POST['title']
-            back_img_id = request.POST['back_id']
-            coordinate.back_img = Back.objects.get(back_img=back_img_id)
-            coordinate.image = photo2image(request.back_id.back_img, request.p.photo)
-            coordinate.save()
-            coordinated_photo = Coordinated.objects.get(title=request.title, photo_id=p)
-            return render(request, 'after.html', {'c_p': coordinated_photo})
-    
-    else:
-        form = CoordinateForm()
-        
-    return render(request, 'coordinate.html', {'p': p, 'backs':backs, 'form':form})
-
-def after(request, pk):
-    image = get_object_or_404(Coordinated, pk=pk)
-    return render(request, 'after.html', {"image":image})
