81d19be4185578232c2737991653057eaefd87fc
TaikiCode <metoro5151@gmail.com>
2021-08-15 08:26:54 +0900

    投稿削除・編集機能done

--- photoremake/urls.py
+++ photoremake/urls.py
@@ -5,6 +5,8 @@ from .import views
 app_name = 'photoremake'
 urlpatterns = [
     path('', views.index, name="index"),
+    path('<pk>/update', views.PhotoUpdateView.as_view(), name='photo-update'),
+    path('<pk>/delete', views.PhotoDeleteView.as_view(), name='photo-delete'),
     path('upload/', views.upload_photo, name="upload"),
     path('upload_image/', views.upload_image, name='upload_image'),
     path('emotion/', views.emotion, name='emotion')
